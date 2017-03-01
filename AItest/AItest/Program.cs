227d1a03c51dc5fd562330f838eaba3f075cd4d0
Blixen <jeppe.blixen@gmail.com>
2017-03-01 17:16:58 +0100

    Fixed main

--- AItest/AItest/Program.cs
+++ AItest/AItest/Program.cs
@@ -18,15 +18,9 @@ namespace AItest
         {
             Map myMap = new Map(10, 10);
 
-            myMap.testMap(myMap);
-
             //Init
-<<<<<<< HEAD
             //var brick = new Brick(new BluetoothCommunication("COM11"), true);
             var brick = new Brick(new UsbCommunication(), true);
-=======
-            /*var brick = new Brick(new BluetoothCommunication("COM11"), true);
->>>>>>> Code for generating map and get distance from position to objects is done
             Connect(brick).Wait();
             System.Threading.Thread.Sleep(100);
             Sonar = new Sensor(brick, true);
@@ -40,12 +34,9 @@ namespace AItest
             System.Threading.Thread.Sleep(2000);
             Car.Move(MotionEnum.Front, brick, 100, 500).Wait();
 
-<<<<<<< HEAD
             Console.ReadKey();
             brick.Disconnect();
-=======
-            Console.ReadKey();*/
->>>>>>> Code for generating map and get distance from position to objects is done
+            Console.ReadKey();
         }
 
         private static async Task Connect(Brick _brick)

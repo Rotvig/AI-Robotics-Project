3faac56e71f1e1299a2aff33e071a97ce9584fca
Rene Rotvig  Jensen <renerotvig@msn.com>
2017-03-19 13:22:26 +0100

    robot motion added to a new thread

--- AI_In_Robotics/AI_In_Robotics/MainWindow.xaml.cs
+++ AI_In_Robotics/AI_In_Robotics/MainWindow.xaml.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using AI_In_Robotics.Classes;
 using AI_In_Robotics.Utils;
 using Lego.Ev3.Core;
-using MathNet.Spatial.Euclidean;
 using Lego.Ev3.Desktop;
 
 namespace AI_In_Robotics
@@ -21,13 +19,13 @@ namespace AI_In_Robotics
         private SensorFusion Sensors;
         private Motion motionControle;
 
-        static int N = 100000;
-        static ParticleFilter filter;
-        static Astar Astar;
-        static Map World;
-        static Bitmap OriginalBitmap;
-        static Bitmap BitmapClone;
-        private int TestCounter = 0;
+        private static readonly int N = 100000;
+        private static ParticleFilter filter;
+        private static Astar Astar;
+        private static Map World;
+        private static Bitmap OriginalBitmap;
+        private static Bitmap BitmapClone;
+        private int TestCounter;
 
 
         public MainWindow()
@@ -75,187 +73,190 @@ namespace AI_In_Robotics
             OriginalBitmap = new Bitmap(259, 130);
             Image.Source = OriginalBitmap.DrawObjects(roadMap);
 
-            BitmapClone = (Bitmap)OriginalBitmap.Clone();
+            BitmapClone = (Bitmap) OriginalBitmap.Clone();
             Image.Source = BitmapClone.Drawparticles(filter.ParticleSet);
         }
 
-        private void KeyEvent(object sender, System.Windows.Input.KeyEventArgs e)
+        private void KeyEvent(object sender, KeyEventArgs e)
         {
-            if (e.Key == System.Windows.Input.Key.C)
+            if (e.Key == Key.C)
             {
                 Sensors.CalibrateSensors();
             }
 
-            if (e.Key == System.Windows.Input.Key.P)
+            if (e.Key == Key.P)
             {
                 TestCounter++;
                 motionControle.RotationScan(Sensors, filter, OriginalBitmap);
             }
 
-            if (e.Key == System.Windows.Input.Key.M)
+            if (e.Key == Key.M)
             {
                 motionControle.motionTest();
             }
 
-            if (e.Key == System.Windows.Input.Key.S)
+            if (e.Key == Key.S)
             {
                 motionControle.RotationScan(Sensors, filter, OriginalBitmap);
             }
 
 
-
             // specific test
-            if (e.Key == System.Windows.Input.Key.H) // Højre
+            if (e.Key == Key.H) // Højre
             {
                 motionControle.PIDTurn(-45);
                 filter.TurnParticlesRight(45);
 
                 //filter.Resample(Sensors.Read());
 
-                BitmapClone = (Bitmap)OriginalBitmap.Clone();
+                BitmapClone = (Bitmap) OriginalBitmap.Clone();
                 Image.Source = BitmapClone.Drawparticles(filter.ParticleSet);
             }
-            if (e.Key == System.Windows.Input.Key.V) // Venstre
+            if (e.Key == Key.V) // Venstre
             {
                 motionControle.PIDTurn(45);
                 filter.TurnParticlesRight(45);
 
                 //filter.Resample(Sensors.Read());
 
-                BitmapClone = (Bitmap)OriginalBitmap.Clone();
+                BitmapClone = (Bitmap) OriginalBitmap.Clone();
                 Image.Source = BitmapClone.Drawparticles(filter.ParticleSet);
             }
-            if (e.Key == System.Windows.Input.Key.F) // Fremad
+            if (e.Key == Key.F) // Fremad
             {
                 motionControle.PIDMove(10);
                 filter.MoveParticles(10);
 
-                BitmapClone = (Bitmap)OriginalBitmap.Clone();
+                BitmapClone = (Bitmap) OriginalBitmap.Clone();
                 Image.Source = BitmapClone.Drawparticles(filter.ParticleSet);
             }
-            if (e.Key == System.Windows.Input.Key.R) // Resample
+            if (e.Key == Key.R) // Resample
             {
                 filter.Resample(Sensors.Read());
 
-                BitmapClone = (Bitmap)OriginalBitmap.Clone();
+                BitmapClone = (Bitmap) OriginalBitmap.Clone();
                 Image.Source = BitmapClone.Drawparticles(filter.ParticleSet);
             }
 
-            if (e.Key == System.Windows.Input.Key.Space)
+            if (e.Key == Key.Space)
             {
-                var position = filter.getPosition();
+                new Thread(StartRobot).Start();
+            }
+        }
 
-                while ((position.X - Astar._goalX) > 5 && (position.Y - Astar._goalY) > 5)
+        private void StartRobot()
+        {
+            var position = filter.getPosition();
+
+            while ((position.X - Astar._goalX) > 5 && (position.Y - Astar._goalY) > 5)
+            {
+                filter.Resample(Sensors.Read()); // Complete run
+                position = filter.getPosition();
+
+                Dispatcher.BeginInvoke(new Action(() =>
                 {
-                    filter.Resample(Sensors.Read()); // Complete run
-                    position = filter.getPosition();
-
-                    Dispatcher.BeginInvoke(new Action(() =>
-                    {
-                        BitmapClone = (Bitmap)OriginalBitmap.Clone();
-                        Image.Source = BitmapClone.Drawparticles(filter.ParticleSet);
-                    }
+                    BitmapClone = (Bitmap) OriginalBitmap.Clone();
+                    Image.Source = BitmapClone.Drawparticles(filter.ParticleSet);
+                }
                     ));
 
 
-                    Movement movement = Astar.FindPath((int)position.X, (int)position.Y);
+                var movement = Astar.FindPath((int) position.X, (int) position.Y);
 
-                    double orientationTaget = 0;
-                    double turnAngle = 0;
+                double orientationTaget = 0;
+                double turnAngle = 0;
 
-                    switch(movement)
-                    {
-                        case Movement.Right:
-                            orientationTaget = 0 * Math.PI;
-                            if (position.theta < Math.PI)
-                            {
-                                turnAngle = (filter.RadToDeg(-position.theta));
-                            }
-                            else
-                            {
-                                motionControle.PIDTurn(filter.RadToDeg(2*Math.PI - position.theta));
-                            }
-                            break;
-                        case Movement.Up:
-                            orientationTaget = 0.5 * Math.PI;
-                            if (position.theta > orientationTaget &&  position.theta < (1.5 * Math.PI))
-                            {
-                                turnAngle = filter.RadToDeg((0.5 * Math.PI) - position.theta);
-                            }
-                            else
-                            {
-                                if(position.theta < Math.PI)
-                                {
-                                    turnAngle = filter.RadToDeg(0.5 * Math.PI - position.theta);
-                                }
-                                else
-                                {
-                                    turnAngle = filter.RadToDeg(0.5 * Math.PI + (position.theta - 1.5 * Math.PI));
-                                }
-                            }
-                            break;
-                        case Movement.Left:
-                            if (position.theta > orientationTaget && position.theta < (2 * Math.PI))
-                            {
-                                turnAngle = filter.RadToDeg(Math.PI - position.theta);
-                            }
-                            else
-                            {
-                                turnAngle = filter.RadToDeg(Math.PI - position.theta);
-                            }
-                            break;
-                        case Movement.Down:
-                            if (position.theta < orientationTaget && position.theta > (0.5 * Math.PI))
-                            {
-                                turnAngle = -(filter.RadToDeg(1.5 * Math.PI - position.theta));
-                            }
-                            else
-                            {
-                                if (position.theta < Math.PI)
-                                {
-                                    turnAngle = filter.RadToDeg(-0.5 * Math.PI - position.theta);
-                                }
-                                else
-                                {
-                                    turnAngle = filter.RadToDeg(1.5 * Math.PI - position.theta);
-                                }
-                            }
-                            break;
-                    }
-
-                    if(Math.Abs(turnAngle) > 5)
-                    {
-                        if(turnAngle > 90)
+                switch (movement)
+                {
+                    case Movement.Right:
+                        orientationTaget = 0*Math.PI;
+                        if (position.theta < Math.PI)
                         {
-                            motionControle.PIDTurn(90);
-                            motionControle.PIDTurn(turnAngle-90);
-                            filter.TurnParticlesRight(turnAngle);
-                        }
-                        else if(turnAngle < -90)
-                        {
-                            motionControle.PIDTurn(-90);
-                            motionControle.PIDTurn(turnAngle + 90);
-                            filter.TurnParticlesRight(turnAngle);
+                            turnAngle = (filter.RadToDeg(-position.theta));
                         }
                         else
                         {
-                            motionControle.PIDTurn(turnAngle);
-                            filter.TurnParticlesRight(turnAngle);
+                            motionControle.PIDTurn(filter.RadToDeg(2*Math.PI - position.theta));
                         }
-                    }
-
-                    motionControle.PIDMove(2);
-                    filter.MoveParticles(2);
-
-                    Dispatcher.BeginInvoke(new Action(() =>
-                    {
-                        BitmapClone = (Bitmap)OriginalBitmap.Clone();
-                        Image.Source = BitmapClone.Drawparticles(filter.ParticleSet);
-                    }
-                    ));
+                        break;
+                    case Movement.Up:
+                        orientationTaget = 0.5*Math.PI;
+                        if (position.theta > orientationTaget && position.theta < (1.5*Math.PI))
+                        {
+                            turnAngle = filter.RadToDeg((0.5*Math.PI) - position.theta);
+                        }
+                        else
+                        {
+                            if (position.theta < Math.PI)
+                            {
+                                turnAngle = filter.RadToDeg(0.5*Math.PI - position.theta);
+                            }
+                            else
+                            {
+                                turnAngle = filter.RadToDeg(0.5*Math.PI + (position.theta - 1.5*Math.PI));
+                            }
+                        }
+                        break;
+                    case Movement.Left:
+                        if (position.theta > orientationTaget && position.theta < (2*Math.PI))
+                        {
+                            turnAngle = filter.RadToDeg(Math.PI - position.theta);
+                        }
+                        else
+                        {
+                            turnAngle = filter.RadToDeg(Math.PI - position.theta);
+                        }
+                        break;
+                    case Movement.Down:
+                        if (position.theta < orientationTaget && position.theta > (0.5*Math.PI))
+                        {
+                            turnAngle = -(filter.RadToDeg(1.5*Math.PI - position.theta));
+                        }
+                        else
+                        {
+                            if (position.theta < Math.PI)
+                            {
+                                turnAngle = filter.RadToDeg(-0.5*Math.PI - position.theta);
+                            }
+                            else
+                            {
+                                turnAngle = filter.RadToDeg(1.5*Math.PI - position.theta);
+                            }
+                        }
+                        break;
                 }
-            }
 
+                if (Math.Abs(turnAngle) > 5)
+                {
+                    if (turnAngle > 90)
+                    {
+                        motionControle.PIDTurn(90);
+                        motionControle.PIDTurn(turnAngle - 90);
+                        filter.TurnParticlesRight(turnAngle);
+                    }
+                    else if (turnAngle < -90)
+                    {
+                        motionControle.PIDTurn(-90);
+                        motionControle.PIDTurn(turnAngle + 90);
+                        filter.TurnParticlesRight(turnAngle);
+                    }
+                    else
+                    {
+                        motionControle.PIDTurn(turnAngle);
+                        filter.TurnParticlesRight(turnAngle);
+                    }
+                }
+
+                motionControle.PIDMove(2);
+                filter.MoveParticles(2);
+
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    BitmapClone = (Bitmap) OriginalBitmap.Clone();
+                    Image.Source = BitmapClone.Drawparticles(filter.ParticleSet);
+                }
+                    ));
+            }
         }
     }
 }
